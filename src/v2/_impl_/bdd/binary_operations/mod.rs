b6c2d2b794ce20b42885f04a8f5a7e3f72ce1a4f
Samuel Pastva <sam.pastva@gmail.com>
2021-07-16 10:48:44 +0200

    Clean up integration with the `u32` algorithm.

--- src/v2/_impl_/bdd/binary_operations/mod.rs
+++ src/v2/_impl_/bdd/binary_operations/mod.rs
@@ -6,7 +6,7 @@ use std::ops::BitXor;
 mod u48;
 
 /// **(internal)** An `apply` algorithm designed for `Bdd` with up to `2^32 - 1`
-/// nodes. The main improvement is lower memory footprint.
+/// nodes. The main improvement is lower memory footprint and hence better caching.
 ///
 /// The performance impact of this is not very drastic, but it is a nice, consistent
 /// 10-15% improvement, so why not do it.
@@ -15,31 +15,92 @@ mod u32;
 impl Bdd {
     /// A logical conjunction of two `Bdd` objects.
     pub fn and(&self, other: &Bdd) -> Bdd {
-        self._u48_and(other) // Symmetric operation
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.and(self)
+        } else {
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_and(other)
+            } else {
+                self._u48_and(other)
+            }
+        }
     }
 
     /// A logical disjunction of two `Bdd` objects.
     pub fn or(&self, other: &Bdd) -> Bdd {
-        self._u48_or(other) // Symmetric operation
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.or(self)
+        } else {
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_or(other)
+            } else {
+                self._u48_or(other)
+            }
+        }
     }
 
     /// A logical implication of two `Bdd` objects.
     pub fn imp(&self, other: &Bdd) -> Bdd {
-        if other.node_count() > self.node_count() {
-            other._u48_inv_imp(self)
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.inv_imp(self)
         } else {
-            self._u48_imp(other)
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_imp(other)
+            } else {
+                self._u48_imp(other)
+            }
+        }
+    }
+
+    /// **(internal)** A mirrored implication operation.
+    fn inv_imp(&self, other: &Bdd) -> Bdd {
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.imp(self)
+        } else {
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_inv_imp(other)
+            } else {
+                self._u48_inv_imp(other)
+            }
         }
     }
 
     /// A logical equivalence of two `Bdd` objects.
     pub fn iff(&self, other: &Bdd) -> Bdd {
-        self._u48_iff(other) // Symmetric operation
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.iff(self)
+        } else {
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_iff(other)
+            } else {
+                self._u48_iff(other)
+            }
+        }
     }
 
     /// A logical exclusive disjunction of two `Bdd` objects.
     pub fn xor(&self, other: &Bdd) -> Bdd {
-        self._u48_xor(other) // Symmetric operation
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.xor(self)
+        } else {
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_xor(other)
+            } else {
+                self._u48_xor(other)
+            }
+        }
     }
 
     /// A logical conjunction with a negated send argument of two `Bdd` objects.
@@ -47,10 +108,31 @@ impl Bdd {
     /// This method is used for set difference when using `Bdd` as a set representation.
     /// That is why it warrants a separate method.
     pub fn and_not(&self, other: &Bdd) -> Bdd {
-        if other.node_count() > self.node_count() {
-            other._u48_not_and(other)
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.not_and(self)
         } else {
-            self._u48_and_not(other)
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_and_not(other)
+            } else {
+                self._u48_and_not(other)
+            }
+        }
+    }
+
+    /// **(internal)** A mirrored `and_not` operation.
+    fn not_and(&self, other: &Bdd) -> Bdd {
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
+            other.and_not(self)
+        } else {
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                self._u32_not_and(other)
+            } else {
+                self._u48_not_and(other)
+            }
         }
     }
 
@@ -65,10 +147,16 @@ impl Bdd {
     where
         TABLE: Fn(NodeId, NodeId) -> NodeId,
     {
-        if other.node_count() > self.node_count() {
+        let self_nodes = self.node_count() as u64;
+        let other_nodes = other.node_count() as u64;
+        if other_nodes > self_nodes {
             other.binary_operation(self, |l, r| table(r, l))
         } else {
-            u48::_u48_apply(self, other, table)
+            if self_nodes < u32::MAX_LEFT_SIZE && other_nodes < u32::MAX_RIGHT_SIZE {
+                u32::_u32_apply(self, other, table)
+            } else {
+                u48::_u48_apply(self, other, table)
+            }
         }
     }
 }
--- src/v2/_impl_/bdd/binary_operations/u32/mod.rs
+++ src/v2/_impl_/bdd/binary_operations/u32/mod.rs
@@ -21,17 +21,126 @@ mod partial_task_cache;
 pub(super) struct PointerPair(u64);
 
 /// Left `Bdd` can be anything that does not overflow `u32`.
-const MAX_LEFT_SIZE: u64 = u32::MAX as u64;
+pub(super) const MAX_LEFT_SIZE: u64 = u32::MAX as u64;
 /// Right `Bdd` cannot have the highest bit set.
-const MAX_RIGHT_SIZE: u64 = MAX_LEFT_SIZE ^ (1 << 31);
+pub(super) const MAX_RIGHT_SIZE: u64 = MAX_LEFT_SIZE ^ (1 << 31);
 
-impl Bdd {
+pub(super) fn _u32_apply<TABLE>(left_bdd: &Bdd, right_bdd: &Bdd, lookup: TABLE) -> Bdd
+where
+    TABLE: Fn(NodeId, NodeId) -> NodeId
+{
+    debug_assert!(left_bdd.node_count() < usize::try_from(MAX_LEFT_SIZE).unwrap());
+    debug_assert!(right_bdd.node_count() < usize::try_from(MAX_RIGHT_SIZE).unwrap());
 
-    pub fn _u32_and_not(&self, other: &Bdd) -> Bdd {
-        debug_assert!(self.node_count() < usize::try_from(MAX_LEFT_SIZE).unwrap());
-        debug_assert!(other.node_count() < usize::try_from(MAX_RIGHT_SIZE).unwrap());
-        let left_bdd = self;
-        let right_bdd = other;
+    let variables = max(left_bdd.variable_count(), right_bdd.variable_count());
+
+    let mut is_not_false = false;
+    let mut node_cache = NodeCache::new(left_bdd.node_count());
+    let mut task_cache = TaskCache::new(left_bdd.node_count(), right_bdd.node_count());
+    let mut stack = Stack::new(variables);
+    unsafe {
+        let root = PointerPair::pack(left_bdd.root_node(), right_bdd.root_node());
+        stack.push_task_unchecked(root);
+    }
+
+    // The calls to stack operations are safe due to the order in which we perform the Bdd search.
+    loop {
+        // If the top is a result, go straight to finishing a task. If not, first expand,
+        // but if the result of the expansion is a finished task, then also finish a task.
+        let mut finish_task = stack.has_result();
+
+        if !finish_task {
+            // Expand current top task.
+            let tasks = unsafe { stack.peek_as_task_unchecked() };
+            let (left, right) = tasks.unpack();
+
+            let lookup_result = lookup(left, right);
+            is_not_false = is_not_false || lookup_result.is_one();
+
+            if !lookup_result.is_undefined() {
+                finish_task = finish_task || unsafe { stack.save_result_unchecked(lookup_result) };
+            } else {
+                let cached_node = task_cache.read(tasks);
+                if !cached_node.is_undefined() {
+                    finish_task =
+                        finish_task || unsafe { stack.save_result_unchecked(cached_node) };
+                } else {
+                    let left_node = unsafe { left_bdd.get_node_unchecked(left) };
+                    let right_node = unsafe { right_bdd.get_node_unchecked(right) };
+                    let (left_var, left_low, left_high) = left_node.unpack();
+                    let (right_var, right_low, right_high) = right_node.unpack();
+                    left_bdd.prefetch(left_low);
+                    right_bdd.prefetch(right_low);
+
+                    let decision_variable = min(left_var, right_var);
+
+                    let (left_low, left_high) = if decision_variable == left_var {
+                        (left_low, left_high)
+                    } else {
+                        (left, left)
+                    };
+
+                    let (right_low, right_high) = if decision_variable == right_var {
+                        (right_low, right_high)
+                    } else {
+                        (right, right)
+                    };
+
+                    let low_tasks = PointerPair::pack(left_low, right_low);
+                    let high_tasks = PointerPair::pack(left_high, right_high);
+
+                    task_cache.prefetch(high_tasks);
+
+                    // When completed, the order of tasks will be swapped (high on top).
+                    unsafe {
+                        stack.push_task_unchecked(high_tasks);
+                        stack.push_task_unchecked(low_tasks);
+                    }
+                }
+            }
+        }
+
+        if finish_task {
+            // Finish current top task.
+            let (low, high) = unsafe { stack.pop_results_unchecked() };
+            let task = unsafe { stack.peek_as_task_unchecked() };
+
+            if high == low {
+                task_cache.write(task, low);
+                unsafe { stack.save_result_unchecked(low) };
+            } else {
+                let (left, right) = task.unpack();
+                let (left_var, right_var) =
+                    (left_bdd.get_variable(left), right_bdd.get_variable(right));
+                let decision_variable = min(left_var, right_var);
+
+                let node = BddNode::pack(decision_variable, low, high);
+                let result_id = node_cache.ensure(node);
+                task_cache.write(task, result_id);
+                unsafe { stack.save_result_unchecked(result_id) };
+            }
+        }
+
+        if stack.has_last_entry() {
+            break; // The last entry is the result to the first task.
+        }
+    }
+
+    if is_not_false {
+        let mut result = node_cache.export();
+        result.update_variable_count(variables);
+        result
+    } else {
+        Bdd::new_false()
+    }
+}
+
+macro_rules! apply_u32 {
+    ($left:ident, $right:ident, $zero:expr, $one:expr) => {{
+        let left_bdd = $left;
+        let right_bdd = $right;
+        debug_assert!(left_bdd.node_count() < usize::try_from(MAX_LEFT_SIZE).unwrap());
+        debug_assert!(right_bdd.node_count() < usize::try_from(MAX_RIGHT_SIZE).unwrap());
 
         let variables = max(left_bdd.variable_count(), right_bdd.variable_count());
 
@@ -55,9 +164,9 @@ impl Bdd {
                 let tasks = unsafe { stack.peek_as_task_unchecked() };
                 let (left, right) = tasks.unpack();
 
-                if left.is_zero() || right.is_one() {
+                if $zero(left, right) {
                     finish_task = finish_task || unsafe { stack.save_result_unchecked(NodeId::ZERO) };
-                } else if left.is_one() && right.is_zero() {
+                } else if $one(left, right) {
                     is_not_false = true;
                     finish_task = finish_task || unsafe { stack.save_result_unchecked(NodeId::ONE) };
                 } else {
@@ -134,6 +243,89 @@ impl Bdd {
         } else {
             Bdd::new_false()
         }
+    }}
+}
+
+impl Bdd {
+
+    pub(super) fn _u32_and(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| l.is_zero() || r.is_zero(),
+            |l: NodeId, r: NodeId| l.is_one() && r.is_one()
+        )
+    }
+
+    pub(super) fn _u32_or(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| l.is_zero() && r.is_zero(),
+            |l: NodeId, r: NodeId| l.is_one() || r.is_one()
+        )
+    }
+
+    pub(super) fn _u32_imp(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| l.is_one() && r.is_zero(),
+            |l: NodeId, r: NodeId| l.is_zero() || r.is_one()
+        )
+    }
+
+    pub(super) fn _u32_inv_imp(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| l.is_zero() || r.is_one(),
+            |l: NodeId, r: NodeId| l.is_one() && r.is_zero()
+        )
+    }
+
+    pub(super) fn _u32_iff(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| (l.is_one() && r.is_zero()) || (l.is_zero() && r.is_one()),
+            |l: NodeId, r: NodeId| (l.is_zero() && r.is_zero()) || (l.is_one() && r.is_one())
+        )
+    }
+
+    pub(super) fn _u32_xor(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| (l.is_one() && r.is_one()) || (l.is_zero() && r.is_zero()),
+            |l: NodeId, r: NodeId| (l.is_zero() && r.is_one()) || (l.is_one() && r.is_zero())
+        )
+    }
+
+    pub(super) fn _u32_and_not(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| l.is_zero() || r.is_one(),
+            |l: NodeId, r: NodeId| l.is_one() && r.is_zero()
+        )
+    }
+
+    pub(super) fn _u32_not_and(&self, other: &Bdd) -> Bdd {
+        debug_assert!(self.node_count() >= other.node_count());
+        apply_u32!(
+            self,
+            other,
+            |l: NodeId, r: NodeId| l.is_one() && r.is_zero(),
+            |l: NodeId, r: NodeId| l.is_zero() || r.is_one()
+        )
     }
 
 }
